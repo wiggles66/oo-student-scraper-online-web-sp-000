39869adbc29a1d7f50e89b6f5eada62923892a17
Michael Waide <mvwaide@gmail.com>
2020-08-05 13:54:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ class Scraper
   def self.scrape_index_page(index_url)
       page = Nokogiri::HTML(open(index_url))
       students = []
-  
+
       page.css("div.student-card").each do |student|
         name = student.css(".student-name").text
         location = student.css(".student-location").text
